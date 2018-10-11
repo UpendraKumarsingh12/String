3733f6e4dead37c29b7908ebaea268829f2591a4
Upendra Singh <Upendra Singh@LAPTOP-0BHM7D51>
2018-10-11 15:27:11 +0530

    palidrome and aragnam

---
+++ gs-rest-service/src/main/java/hello/Palindrome.java
@@ -0,0 +1,41 @@
+package hello;
+
+public class Palindrome {
+
+    private String word;
+    private boolean palindrome;
+    private boolean anagramOfPalindrome;
+
+    public Palindrome(String word, boolean palindrome, boolean anagramOfPalindrome) {
+        this.word = word;
+        this.palindrome = palindrome;
+        this.anagramOfPalindrome = anagramOfPalindrome;
+    }
+
+    public Palindrome() {
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public boolean isPalindrome() {
+        return palindrome;
+    }
+
+    public void setPalindrome(boolean palindrome) {
+        this.palindrome = palindrome;
+    }
+
+    public boolean isAnagramOfPalindrome() {
+        return anagramOfPalindrome;
+    }
+
+    public void setAnagramOfPalindrome(boolean anagramOfPalindrome) {
+        this.anagramOfPalindrome = anagramOfPalindrome;
+    }
+}
