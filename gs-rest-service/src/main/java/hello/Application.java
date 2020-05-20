3f7c003977fea5a190d6bbbd5cb338f8b9d5fff1
UpendraKumarSingh12 <upendra.singh001989@gmail.com>
2020-05-20 09:36:32 +0530

    push and commit

--- gs-rest-service/src/main/java/hello/Application.java
+++ gs-rest-service/src/main/java/hello/Application.java
@@ -2,7 +2,7 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// testing for git repository
+// testing for git repository for commoit and push
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
