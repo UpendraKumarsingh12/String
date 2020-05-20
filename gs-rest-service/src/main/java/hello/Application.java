505681e37ef765633172fc4d507ce3b2d1f68e5b
UpendraKumarSingh12 <upendra.singh001989@gmail.com>
2020-05-20 09:33:43 +0530

    for testing pupose

--- gs-rest-service/src/main/java/hello/Application.java
+++ gs-rest-service/src/main/java/hello/Application.java
@@ -2,10 +2,11 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// testing for git repository
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        sysou
     }   
 }
